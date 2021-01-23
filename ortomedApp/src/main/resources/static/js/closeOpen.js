6961678079a4aa51e2edbc52809647bb9913e4b7
unknown <waly@gmail.com>
2021-01-23 14:36:52 +0100

    OpenClosed fixed bug

--- ortomedApp/src/main/resources/static/js/closeOpen.js
+++ ortomedApp/src/main/resources/static/js/closeOpen.js
@@ -1,13 +1,15 @@
 window.onload = function () {
     var status = document.getElementById("status");
     var today = new Date(Date.now());
-    if(today.getDay() != 0 || today.getDay() != 6){
-        if(today.getHours() >= 7 && today.getHours() < 22){
-            console.log("OPEN");
-            status.innerHTML = "!! OTWARTE !!";
-            setInterval(function () {
-                status.classList.toggle("open");
-            }, 1000);
+    if(today.getDay() != 0){
+        if(today.getDay() != 6) {
+            if (today.getHours() >= 7 && today.getHours() < 21) {
+                console.log("OPEN");
+                status.innerHTML = "!! OTWARTE !!";
+                setInterval(function () {
+                    status.classList.toggle("open");
+                }, 1000);
+            }
         }
         else {
             console.log("CLOSED");
